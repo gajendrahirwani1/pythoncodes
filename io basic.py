ccf29973439352b01254291b8a9be1229691e11d
Gajendra Hirwani <gajendrahirwani1@gmail.com>
2022-11-20 23:35:57 +0530

    Create io basic.py

---
+++ io basic.py
@@ -0,0 +1,10 @@
+# file io basic
+"""
+"r" - open file for reading (default mode)
+"w" - open file for writing
+"x" - create file if not exists
+"a" - add more content to a file
+"t" - text mode   (default mode)
+"b" - binary mode
+"+" - read and write mode
+"""
