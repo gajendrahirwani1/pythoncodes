0499e13e6df9b8f0e10c97af3788c69b734da200
Gajendra Hirwani <gajendrahirwani1@gmail.com>
2022-11-20 23:26:49 +0530

    Create for else.py

---
+++ for else.py
@@ -0,0 +1,16 @@
+#for-else
+
+khana = ["roti","sbji","chawal"]
+# for item in khana:
+#     if item == "rotiroll":
+#         break
+#
+# else:
+#     print("your item not found")
+for item in khana:
+    if item == "roti":
+        print("yess found")
+        break
+
+else:
+    print("your item no found")
