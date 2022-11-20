ded84301c53941b0745c9e3c2f3ba0538aa1462c
Gajendra Hirwani <gajendrahirwani1@gmail.com>
2022-11-20 23:37:41 +0530

    Create join function.py

---
+++ join function.py
@@ -0,0 +1,11 @@
+# join function
+
+lst = ["sachin","mahi","kohli","shami","sky"]
+for item in lst:
+    print(item,"and", end=" ")
+
+print("other cricket players")       #normal joining elements
+
+
+a = " , ".join(lst)
+print(a, "other cricket player")        #join function
