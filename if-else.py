9230d81a917e6a39f246b99295daed2d21ca3893
Gajendra Hirwani <gajendrahirwani1@gmail.com>
2022-11-20 23:34:01 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,21 @@
+var1 = 10
+#var2 = int(input("enter any number: "))
+
+
+
+list = [1,2,3,4,5,6,7,8]
+print(8 in list)
+if 5 in list:
+    print("yes it is ")
+print(10 not in list)
+if 10 not in list:
+    print("no it is not")
+
+
+
+#exercise
+age1 = int(input("enter your age: "))
+if 17<age1<100:
+    print("you can drive")
+else:
+    print("you can't drive")
