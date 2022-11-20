2f475829befd5df76e21eafcfe1bb67bb631627b
Gajendra Hirwani <gajendrahirwani1@gmail.com>
2022-11-20 23:34:51 +0530

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,18 @@
+#module
+
+import math
+import random
+def functions():
+def display():
+    print('earlier rich owned cars,while poor had horses')
+def show():
+    print('now everyone has car, while only rich own horses')
+a = 100
+b = 200
+print(__name__)
+print(math.sin(0.5))
+print(math.cos(0.5))
+print(random.random())
+print(random.randint(30,45))
+functions.display()
+functions.show()
